9a3aed5fc419a3917f47ccbfa4efbb4673599ae1
spideyzac <zacmlesser-7@outlook.com>
2026-02-12 09:58:54 -0800

    fix: remove uneeded

--- dist/release/0.0.0/mixin.js
+++
@@ -1,7 +0,0 @@
-export default function mixin() {
-    window.polyModLoader.registerGlobalMixin(
-        3,
-        "}), wR(this, qI, 'f').addFinishCallback(e => {",
-        "window.trackMartialOnRaceFinish(e.getTime().numberOfFrames.toString(), wR(this, gI, 'f').getID());"
-    );
-}
